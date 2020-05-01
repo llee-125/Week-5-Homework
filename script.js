0f8dba97a02a19e56717c4123ca328fc096edef4
llee-125 <llee@berkeley.edu>
2020-05-01 16:38:30 -0700

    Starting psudocode working with Hannah.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-console.log ("JQ is hooked up properly");
-
 $(document).ready(function () {
   console.log ("JQ is hooked up properly");
 
 
+// loop through 12 times to create 12 rows, 3 columns  
+for (i = 0; i < 12; i++){
+// create 1 row
+var row = 
+  // define the div
+  // add the row class
+  // for loop for 3 x column
+    // define the div
+    // add the column class
+
+
+}
+
+
 
 });
+
