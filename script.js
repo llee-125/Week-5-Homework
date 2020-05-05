12d5c8b63ad98468cba982ea8f78e14164c05a49
llee-125 <llee@berkeley.edu>
2020-05-05 16:30:40 -0700

    Basic structure and psydocode added from TA and Hannah help.

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 $(document).ready(function () {
   console.log ("JQ is hooked up properly");
 
+  var date = moment().format("MMM Do YYYY");
+  console.log(date);
 
 // loop through 12 times to create 12 rows, 3 columns  
-for (i = 0; i < 12; i++){
+// for (var i = 0; i < 12; i++){
 // create 1 row
-var row = 
+/* var row = document.createElement("tr")
+$(".table1").append(row); */
+
   // define the div
   // add the row class
-  // for loop for 3 x column
+  // for loop for j x column
+  //for (var j = 0; j < 3; j++){
     // define the div
     // add the column class
+ // };
+//}
 
-
-}
-
-
-
+// Want the user to be able to edit the rows
+  // Fix the HTML so middle column are all textareas
+// Want the inputs to be saved in local storage
+  // Create a button on the last column that when you click it, inputs will save
+  // When you click it: Need to add an event listener
+  $(".btn-sv").on("click", function() {
+    // Now I want to add local storage
+    var entry = $(".entry").val();
+    localStorage.setItem("entry", entry);
+  })
+// Want the colors to represent past, present, future
+  // If the time is less than current, it is past
+  // If the time is greater than current, it is future
+  // If time is equal to current, it is present
 });
-
